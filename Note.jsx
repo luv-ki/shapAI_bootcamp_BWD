5d235528a3223702d1fbc7f74186a4636b8bbeba
luv-ki <85449528+luv-ki@users.noreply.github.com>
2021-06-06 22:43:12 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className = 'note'>
+      <p>the shapeAI class was awesome by sharya sir</p>
+    </div>
+  );
+}
+
+export default Note;
